fa13bc4327a6b6bdf1b666f10a3d1a23da6992e3
Victor Ordozgoite <victorordozgoite@Victors-MacBook-Air.local>
2023-01-14 17:48:10 -0400

    Validate POST API

--- routes/books.js
+++ routes/books.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/books');
+const { Book, validateBook } = require('../models/books');
 
 // POST: CREATE A NEW BOOK
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const error = await validateBook(req.body);
+  if (error.message) {
+    res.status(400).send(error.message);
+  } else {
     const book = new Book({
       name: req.body.bookName,
       author: {
         name: req.body.authorName,
         age: req.body.authorAge
       },
-      genre: req.body.genre  
+      genre: req.body.genre
     });
-
+  
     book.save().then(book => {
-        res.send(book);
+      res.send(book);
     }).catch(error => {
-        res.status(500).send("Book was not stored in DB.");
+      res.status(500).send("Book was not stored in DB.");
     });
+  }
 });
 
 module.exports = router;
