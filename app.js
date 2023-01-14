c0717c87bd5513e35562485e76113f09d9ab4f91
Victor Ordozgoite <victorordozgoite@Victors-MacBook-Air.local>
2023-01-13 20:45:46 -0400

    Added a POST API (Book)

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const booksRoute = require('./routes/books');
 
 const app = express();
 const port = 3000;
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// routes
+app.use('/api/books', booksRoute);
 
 // connect to mongodb atlas
 mongoose.connect("mongodb+srv://admin-victor:Test123@cluster0.ucxvmno.mongodb.net/booksdb?retryWrites=true&w=majority", {useNewUrlParser: true}).then(() => {
