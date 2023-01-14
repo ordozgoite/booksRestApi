f8ba6a02db92f35777c1cd376c07ae58e216d052
Victor Ordozgoite <victorordozgoite@Victors-MacBook-Air.local>
2023-01-14 19:03:47 -0400

    Added GET, PUT and DELETE routes

--- routes/books.js
+++ routes/books.js
@@ -16,12 +16,61 @@ router.post('/', async (req, res) => {
       },
       genre: req.body.genre
     });
-  
+
     book.save().then(book => {
       res.send(book);
     }).catch(error => {
       res.status(500).send("Book was not stored in DB.");
     });
+  }
+});
+
+// GET ALL BOOKS
+router.get("/", (req, res) => {
+  Book.find()
+    .then(books => res.send(books))
+    .catch((error) => {
+      res.status(500).send("Something went wrong");
+    });
+});
+
+// GET BOOK BY ID
+router.get("/:bookId", async (req, res) => {
+  const book = await Book.findById(req.params.bookId)
+  if (!book) {
+    res.status(404).send("Book not found.");
+  } else {
+    res.send(book);
+  }
+});
+
+// UPDATE BOOK BASED ON ID
+router.put("/:bookId", async (req, res) => {
+  const updatedBook = await Book.findByIdAndUpdate(req.params.bookId, {
+    name: req.body.bookName,
+    author: {
+      name: req.body.authorName,
+      age: req.body.authorAge
+    },
+    genre: req.body.genre
+  },
+    { new: true }
+  );
+
+  if (!updatedBook) {
+    res.status(404).send("Book not found.");
+  } else {
+    res.send(updatedBook);
+  }
+});
+
+// DELETE BOOK BASED ON ID
+router.delete("/:bookId", async (req, res) => {
+  const book = await Book.findByIdAndRemove(req.params.bookId);
+  if (!book) {
+    res.status(404).send("Book not found");
+  } else {
+    res.send("Book was deleted.");
   }
 });
 
