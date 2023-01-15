d5baf3882f4472f911590ac7149608217f4a3a94
Victor Ordozgoite <victorordozgoite@Victors-MacBook-Air.local>
2023-01-14 20:16:57 -0400

    Added winston logger to handle errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const booksRoute = require('./routes/books');
+const winston = require('winston');
 
 const app = express();
 const port = 3000;
@@ -9,16 +10,34 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// create a logger
+const logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize({all: true})
+      )
+    }),
+    new winston.transports.File({ filename: 'error.log', level: 'error' })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'exceptions.log' })
+  ]
+});
+
 // routes
 app.use('/api/books', booksRoute);
 
 // connect to mongodb atlas
-mongoose.connect("mongodb+srv://admin-victor:Test123@cluster0.ucxvmno.mongodb.net/booksdb?retryWrites=true&w=majority", {useNewUrlParser: true}).then(() => {
-    console.log("Connected to mongodb atlas.");
+mongoose
+  .connect("mongodb+srv://admin-victor:Test123@cluster0.ucxvmno.mongodb.net/booksdb?retryWrites=true&w=majority", {useNewUrlParser: true})
+  .then(() => {
+    logger.info("Connected to MongoDB Atlas.");
 }).catch(error => {
-    console.log("Something wrong happened", error);
+    logger.error(error.message);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  logger.info(`Example app listening on port ${port}`);
 });
